84317974be9e4167657cfcf85eba624b0947a511
jerome.jouanny <jouanny.jerome@gmail.com>
2017-04-20 15:01:34 +0200

    base de donnée

--- tableprojet.sql
+++ tableprojet.sql
@@ -1,63 +1,211 @@
 drop database if exists gestion_planning;
 create database gestion_planning;
-
 use gestion_planning;
-create table salle(
-	id_salle int(3)  auto_increment,
-	numero_salle varchar(55) not null,
-	capacite int (3) not null,	
-	primary key (id_salle)
-)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+--
 
-create table classe(
-	id_classe int(3) auto_increment,
-	nb_eleve int(3) not null,
-	niveau varchar(100) not null,
-	primary key (id_classe)
+CREATE TABLE `Admin` (
+  `id_admin` int(3) NOT NULL,
+  `nom` varchar(100) NOT NULL,
+  `prenom` varchar(100) NOT NULL,
+  `identifiant` varchar(100) NOT NULL,
+  `mdp` varchar(55) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+--
+-- Contenu de la table `Admin`
+--
 
-create table professeur(
-	id_prof int(3)  auto_increment,
-	nom varchar(100) not null,
-	prenom varchar(100) not null,
-	identifiant varchar(100) not null,
-	mdp varchar(55) not null,
-	primary key (id_prof)
-)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+INSERT INTO `Admin` (`id_admin`, `nom`, `prenom`, `identifiant`, `mdp`) VALUES
+(1, 'Maïssane', 'Yagoubi', 'Maissane', 'Perrier'),
+(2, 'Amaskane', 'Elias', 'unibet', 'test');
 
-create table matiere(
-	id_matiere int(3)  auto_increment,
-	libelle varchar(100) not null,
-	primary key (id_matiere)
-)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+-- --------------------------------------------------------
 
-create table affectation(
-	id_affectation int(3)  auto_increment,
-	id_prof int(3) not null,
-	id_matiere int(3) not null,
-	primary key (id_affectation),
-	foreign key (id_prof) references professeur (id_prof),
-	foreign key (id_matiere) references matiere (id_matiere)
-)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+--
+-- Structure de la table `affectation`
+--
 
-create table cours(
-	id_cours int(3)  auto_increment,
-	libelle varchar (55) not null,
-	id_salle int(3) not null,
-	id_classe int(3) not null,
-	id_affectation int(3) not null,
-	primary key (id_cours),
-	foreign key (id_affectation) references professeur (id_prof),
-	foreign key (id_salle) references salle (id_salle),
-	foreign key (id_classe) references classe (id_classe)
-)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE `affectation` (
+  `id_affectation` int(3) NOT NULL,
+  `id_prof` int(3) NOT NULL,
+  `id_matiere` int(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-create table Admin(
-	id_admin int(3)  auto_increment,
-	nom varchar(100) not null,
-	prenom varchar(100) not null,
-	identifiant varchar(100) not null,
-	mdp varchar(55) not null,
-	primary key (id_admin)
-)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `classe`
+--
+
+CREATE TABLE `classe` (
+  `id_classe` int(3) NOT NULL,
+  `nb_eleve` int(3) NOT NULL,
+  `niveau` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `cours`
+--
+
+CREATE TABLE `cours` (
+  `id_cours` int(3) NOT NULL,
+  `libelle` varchar(55) NOT NULL,
+  `id_salle` int(3) NOT NULL,
+  `id_classe` int(3) NOT NULL,
+  `id_affectation` int(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `matiere`
+--
+
+CREATE TABLE `matiere` (
+  `id_matiere` int(3) NOT NULL,
+  `libelle` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `professeur`
+--
+
+CREATE TABLE `professeur` (
+  `id_prof` int(3) NOT NULL,
+  `nom` varchar(100) NOT NULL,
+  `prenom` varchar(100) NOT NULL,
+  `identifiant` varchar(100) NOT NULL,
+  `mdp` varchar(55) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Contenu de la table `professeur`
+--
+
+INSERT INTO `professeur` (`id_prof`, `nom`, `prenom`, `identifiant`, `mdp`) VALUES
+(1, 'Yagoubi', 'Maïssane', 'Maïssou', 'Perrier');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `salle`
+--
+
+CREATE TABLE `salle` (
+  `id_salle` int(3) NOT NULL,
+  `numero_salle` varchar(55) NOT NULL,
+  `capacite` int(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Index pour les tables exportées
+--
+
+--
+-- Index pour la table `Admin`
+--
+ALTER TABLE `Admin`
+  ADD PRIMARY KEY (`id_admin`);
+
+--
+-- Index pour la table `affectation`
+--
+ALTER TABLE `affectation`
+  ADD PRIMARY KEY (`id_affectation`),
+  ADD KEY `id_prof` (`id_prof`),
+  ADD KEY `id_matiere` (`id_matiere`);
+
+--
+-- Index pour la table `classe`
+--
+ALTER TABLE `classe`
+  ADD PRIMARY KEY (`id_classe`);
+
+--
+-- Index pour la table `cours`
+--
+ALTER TABLE `cours`
+  ADD PRIMARY KEY (`id_cours`),
+  ADD KEY `id_affectation` (`id_affectation`),
+  ADD KEY `id_salle` (`id_salle`),
+  ADD KEY `id_classe` (`id_classe`);
+
+--
+-- Index pour la table `matiere`
+--
+ALTER TABLE `matiere`
+  ADD PRIMARY KEY (`id_matiere`);
+
+--
+-- Index pour la table `professeur`
+--
+ALTER TABLE `professeur`
+  ADD PRIMARY KEY (`id_prof`);
+
+--
+-- Index pour la table `salle`
+--
+ALTER TABLE `salle`
+  ADD PRIMARY KEY (`id_salle`);
+
+--
+-- AUTO_INCREMENT pour les tables exportées
+--
+
+--
+-- AUTO_INCREMENT pour la table `Admin`
+--
+ALTER TABLE `Admin`
+  MODIFY `id_admin` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT pour la table `affectation`
+--
+ALTER TABLE `affectation`
+  MODIFY `id_affectation` int(3) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `classe`
+--
+ALTER TABLE `classe`
+  MODIFY `id_classe` int(3) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `cours`
+--
+ALTER TABLE `cours`
+  MODIFY `id_cours` int(3) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `matiere`
+--
+ALTER TABLE `matiere`
+  MODIFY `id_matiere` int(3) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `professeur`
+--
+ALTER TABLE `professeur`
+  MODIFY `id_prof` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT pour la table `salle`
+--
+ALTER TABLE `salle`
+  MODIFY `id_salle` int(3) NOT NULL AUTO_INCREMENT;
+--
+-- Contraintes pour les tables exportées
+--
+
+--
+-- Contraintes pour la table `affectation`
+--
+ALTER TABLE `affectation`
+  ADD CONSTRAINT `affectation_ibfk_1` FOREIGN KEY (`id_prof`) REFERENCES `professeur` (`id_prof`),
+  ADD CONSTRAINT `affectation_ibfk_2` FOREIGN KEY (`id_matiere`) REFERENCES `matiere` (`id_matiere`);
+
+--
+-- Contraintes pour la table `cours`
+--
+ALTER TABLE `cours`
+  ADD CONSTRAINT `cours_ibfk_1` FOREIGN KEY (`id_affectation`) REFERENCES `professeur` (`id_prof`),
+  ADD CONSTRAINT `cours_ibfk_2` FOREIGN KEY (`id_salle`) REFERENCES `salle` (`id_salle`),
+  ADD CONSTRAINT `cours_ibfk_3` FOREIGN KEY (`id_classe`) REFERENCES `classe` (`id_classe`);
