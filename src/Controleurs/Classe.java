dd833c6641a8d678eaf17c684c863cd792d5da84
jerome.jouanny <jouanny.jerome@gmail.com>
2017-05-22 15:42:19 +0200

    jayjay

--- src/Controleurs/Classe.java
+++ src/Controleurs/Classe.java
@@ -1,5 +1,59 @@
 package Controleurs;
 
-public class Classe {
+public class Classe 
+{
+	int id_classe;
+	int nombreEleve;
+	String specialite, niveau;
+	
+	public Classe()
+	{
+		id_classe = 0;
+		nombreEleve = 0;
+		specialite = "";
+		niveau = "";
+		
+	}
+	
+	public Classe(int id_classe, int nombreEleve, String specialite, String niveau)
+	{
+		this.id_classe = id_classe;
+		this.nombreEleve = nombreEleve;
+		this.specialite = specialite;
+		this.niveau = niveau;
+	}
 
+	public int getId_classe() {
+		return id_classe;
+	}
+
+	public void setId_classe(int id_classe) {
+		this.id_classe = id_classe;
+	}
+
+	public int getNombreEleve() {
+		return nombreEleve;
+	}
+
+	public void setNombreEleve(int nombreEleve) {
+		this.nombreEleve = nombreEleve;
+	}
+
+	public String getSpecialite() {
+		return specialite;
+	}
+
+	public void setSpecialite(String specialite) {
+		this.specialite = specialite;
+	}
+
+	public String getNiveau() {
+		return niveau;
+	}
+
+	public void setNiveau(String niveau) {
+		this.niveau = niveau;
+	}
+	
+	
 }
--- src/Modeles/Modele_Classe.java
+++ src/Modeles/Modele_Classe.java
@@ -1,5 +1,75 @@
 package Modeles;
 
-public class Modele_Classe {
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.LinkedList;
 
+import javax.swing.JOptionPane;
+
+import com.mysql.jdbc.Statement;
+
+import Controleurs.Classe;
+
+public class Modele_Classe {
+	public static LinkedList<Classe> selectall()
+	{
+		String requete ="Select * from classe;";
+		LinkedList<Classe> uneListe = new LinkedList<Classe>();
+		
+		Modele unModele = new Modele("127.0.0.1", "gestion_planning", "root", "");
+		unModele.connexion();
+		try{
+			Statement unStat =  (Statement) unModele.getMaConnexion().createStatement();
+			
+			ResultSet unRes = unStat.executeQuery(requete);
+			
+			while(unRes.next())
+			{
+				int id_classe = unRes.getInt("id_classe");
+				int nombreEleve = unRes.getInt("nombreEleve");
+				String specialite = unRes.getString("specialite");
+				String niveau = unRes.getString("niveau");
+			
+				Classe unP = new Classe(id_classe, nombreEleve, specialite, niveau);
+				uneListe.add(unP);
+			}
+			unStat.close();
+		}
+		catch (SQLException exp)
+		{
+			JOptionPane.showMessageDialog(null, "Erreure :"+ exp);
+		}
+		unModele.deconnexion();
+		return uneListe;		
+	}
+	
+	public static Classe selectwhere(int idclasse)	
+	{
+		String requete ="Select * from classe where id_classe = '"+idclasse+"';";
+		Classe unP = null;
+		Modele unModele = new Modele("127.0.0.1", "gestion_planning", "root", "");
+		unModele.connexion();
+		try{
+			java.sql.Statement unStat =  unModele.getMaConnexion().createStatement();
+			
+			ResultSet unRes = unStat.executeQuery(requete);
+			
+			if(unRes.next())
+			{
+				int id_classe = unRes.getInt("id_classe");
+				int nombreEleve = unRes.getInt("nombreEleve");
+				String specialite = unRes.getString("specialite");
+				String niveau = unRes.getString("niveau");
+				unP = new Classe(id_classe, nombreEleve, specialite, niveau);
+			}
+			
+			unStat.close();
+		}
+		catch (SQLException exp)
+		{
+			JOptionPane.showMessageDialog(null, "Erreur :"+ exp);
+		}
+		unModele.deconnexion();
+		return unP;		
+	}
 }
