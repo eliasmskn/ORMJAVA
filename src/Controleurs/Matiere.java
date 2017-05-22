6ef56304860c00beb9563140b5b42840d9eb5350
Maïssane yagoubi <maissane@MacBook-Pro-de-Maissane.local>
2017-05-22 15:37:34 +0200

    vive la nasa

--- src/Controleurs/Matiere.java
+++ src/Controleurs/Matiere.java
@@ -2,4 +2,25 @@ package Controleurs;
 
 public class Matiere {
 
+	int id_matiere;
+	String libelle;
+	
+	public Matiere()
+	{
+		id_matiere = 0;
+		libelle = "";
+	}
+	
+	public Matiere(int id_matiere, String libelle)
+	{
+		this.id_matiere = id_matiere;
+		this.libelle = libelle;
+	}
+	
+	public String toString()
+	{
+		return "\n Id Matière:"+this.id_matiere+
+				"\n Libellé Matière :" +this.libelle;
+	}
+
 }
